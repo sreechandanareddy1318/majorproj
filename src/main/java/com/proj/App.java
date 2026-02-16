5cade807969abd90bdc8bf8b00cda9b3ef1cfcd8
sreechandanareddy1318 <chandanareddy1318@gmail.com>
2026-02-16 21:14:39 +0530

    Delete src directory

--- src/main/java/com/proj/App.java
+++
@@ -1,9 +0,0 @@
-package com.sree;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Sree 🚀");
-        System.out.println("Java CI/CD Project Running Successfully!");
-    }
-}
-
